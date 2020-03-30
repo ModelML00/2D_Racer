ae3567461451f2858d1845877702b6f9db395de9
ModelML00 <47201662+ModelML00@users.noreply.github.com>
2020-03-30 02:59:13 -0400

    Main_Push

---
+++ Racer/Racer/Assets/Prototyping/Scripts/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public Transform camRig;
+
+    public float followDistance;
+
+    public float cameraAngle;
+    public float cameraHeight;
+
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3(transform.position.x, cameraHeight, followDistance) - target.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.rotation = target.rotation;
+        transform.position = target.position;
+    }
+
+    [ContextMenu("Set Camera")]
+    public void SetCamera() {
+        transform.rotation = target.rotation;
+        transform.position = target.position;
+        camRig.localPosition = new Vector3(0f, cameraHeight, -followDistance);
+        camRig.localRotation = Quaternion.Euler(cameraAngle, 0f, 0f);
+    }
+}
---
+++ Racer/Racer/Assets/Prototyping/Scripts/Movement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float topSpeed;
+    public float reverseSpeed;
+    public float acceleration;
+    public float decceleration;
+
+    public float wheelSpeed;
+    public float handling;
+
+    public Rigidbody rb;
+
+    float currentSpeed;
+    float currrentDirection;
+
+    float accelVel;
+    float decelVel;
+
+    float rotationVel;
+
+
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space) && !Input.GetKey(KeyCode.LeftControl)) {
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, topSpeed, ref accelVel, acceleration * Time.deltaTime);
+        }
+        if(!Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.LeftControl)) {
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, -reverseSpeed, ref decelVel, decceleration * Time.deltaTime);
+        }
+        if(!Input.GetKey(KeyCode.Space) && !Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.LeftControl)){
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, 0f, ref decelVel, decceleration * Time.deltaTime);
+        }
+
+
+        if(Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) {
+            currrentDirection = Mathf.SmoothDamp(currrentDirection, -1f, ref rotationVel, wheelSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)) {
+            currrentDirection = Mathf.SmoothDamp(currrentDirection, 1f, ref rotationVel, wheelSpeed * Time.deltaTime);
+        }
+        if(!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) {
+            currrentDirection = Mathf.SmoothDamp(currrentDirection, 0f, ref rotationVel, wheelSpeed * Time.deltaTime);
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+
+
+        
+        Vector3 force = transform.forward * currentSpeed;
+
+        rb.AddForce(force);
+
+
+
+        float turn = handling * currrentDirection;
+
+        turn = turn * (currentSpeed / topSpeed);
+
+        Quaternion rotation = transform.rotation * Quaternion.Euler(0f, turn, 0f);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10f);
+
+    }
+
+
+}
