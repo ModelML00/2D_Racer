31329febb3a1381b3c1011a2af287f1f14c5854d
ModelML00 <47201662+ModelML00@users.noreply.github.com>
2020-04-07 13:27:24 -0400

    Added_Kill_Barrier

---
+++ Racer/Assets/Prototyping/Scripts/KillBarrier.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBarrier : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform respawnPosition;
+
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            player.position = respawnPosition.position;
+        }
+    }
+
+}
