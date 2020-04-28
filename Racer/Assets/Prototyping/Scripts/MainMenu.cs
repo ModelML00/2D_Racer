9c8d76cefb0927ee354c4979a9350d02f66ed026
ModelML00 <47201662+ModelML00@users.noreply.github.com>
2020-04-27 23:45:57 -0400

    Menus&Finish

---
+++ Racer/Assets/Prototyping/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject credits;
+
+    public void LoadLevel(int level) {
+        SceneManager.LoadScene(level);
+    }
+
+    public void CreditsScreen() {
+        credits.SetActive(true);
+        menu.SetActive(false);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void Back() {
+        credits.SetActive(false);
+        menu.SetActive(true);
+    }
+}
---
+++ Racer/Assets/Prototyping/Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pausePanel;
+    public GameObject quitPrompt;
+
+    bool gameIsPaused;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if(!gameIsPaused) {
+                Pause();
+            } else {
+                Resume();
+            }
+        }
+    }
+
+    void Pause() {
+        gameIsPaused = true;
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume() {
+        gameIsPaused = false;
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitPrompt() {
+        if(!quitPrompt.activeInHierarchy) {
+            quitPrompt.SetActive(true);
+        } else {
+            quitPrompt.SetActive(false);
+        }
+
+    }
+
+    public void QuitMenu() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitDesktop() {
+        Application.Quit();
+    }
+}
